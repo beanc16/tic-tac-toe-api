97479603d55873b7481915e6c18f7b7285dc9efc
beanc16 <bean.christopher@outlook.com>
2021-10-10 16:12:24 -0400

    Fix bug that caused BoardRow to not be deserialized

--- Models/BoardRow.cs
+++ Models/BoardRow.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 using MongoDB.Bson.Serialization.Attributes;
 using TicTacToeApi.Models.Enums;
 
@@ -6,15 +9,49 @@ namespace TicTacToeApi.Models
     public class BoardRow
     {
         [BsonElement("columns")]
-        public string[] Columns { get; private set; }
+        // public string[] Columns { get; private set; }
+        public List<string> Columns { get; private set; }
 
         public BoardRow()
         {
-            Columns = new string[] {
+            Columns = new List<string>(new string[] {
                 BoardMark.EMPTY,
                 BoardMark.EMPTY,
                 BoardMark.EMPTY,
-            };
+            });
+        }
+
+        [JsonConstructor]   // Called on JsonConvert.DeserializeObject
+        public BoardRow(List<string> columns)
+        {
+            Predicate<string> isValidBoardValue = 
+                (col) => (col == null || 
+                          col.ToLower() == "null" || 
+                          col.ToUpper() == "X" || 
+                          col.ToUpper() == "O");
+            
+            /* Is not null, 
+             * a list with exactly 3 values, 
+             * and a list with all elements that are either:
+             * - "X"
+             * - "O"
+             * - "null"
+             * - null
+             */
+            if (columns != null && columns.Count == 3 && 
+                columns.TrueForAll(isValidBoardValue))
+            {
+                Columns = columns;
+            }
+
+            else
+            {
+                Columns = new List<string>(new string[] {
+                    BoardMark.EMPTY,
+                    BoardMark.EMPTY,
+                    BoardMark.EMPTY,
+                });
+            }
         }
 
 
@@ -36,11 +73,11 @@ namespace TicTacToeApi.Models
             foreach (string column in Columns)
             {
                 string col = column;
-                if (column == null)
+                if (column == null || column == "null")
                 {
-                    col = "null,";
+                    col = "null";
                 }
-                columnStr += "\t" + col + "\n";
+                columnStr += "\t\t" + col + ",\n";
             }
             columnStr += "\t]";
 
