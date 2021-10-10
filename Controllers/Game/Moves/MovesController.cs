b365c390bb65a32293707f130c80f16a8586daf7
beanc16 <bean.christopher@outlook.com>
2021-10-10 18:03:14 -0400

    Add ability to make moves

--- Controllers/Game/Moves/MovesController.cs
+++ Controllers/Game/Moves/MovesController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
 using TicTacToeApi.Models;
 using TicTacToeApi.Models.Enums;
 using MongoDBHelpers;
+using HttpRequestHelpers;
 
 namespace TicTacToeApi.Controllers
 {
+    // TODO: Better error handling
     public class MovesController : Controller
     {
         // /game/{gameId}/moves
@@ -32,7 +36,59 @@ namespace TicTacToeApi.Controllers
             }
         }
 
-        // TODO: Post - Make a move in a game (only if it's your turn)
+        /* TODO:
+         * - moveHistory Rules
+         *      - Can only make one move at a time
+         *      - Can only make a move if it's your turn
+         *          - Update who's turn it is in player data
+         * - Win detection
+         *      - Bool function in Game & Board
+         *      - Add isWinner field to player
+         * - AI
+         *      - Add useApiCpu to player
+         *      - Make CPU take a turn after non-CPU player goes if useApiCpu is true
+         */
         // /game/{gameId}/moves
+        [HttpPatch]
+        public async Task<ActionResult> Index(string gameId, string _)
+        {
+            string reqStr = await Request.GetRawBodyStringAsync();
+
+            // Sent raw data
+            if (reqStr.Length > 0)
+            {
+                JObject obj = JsonConvert.DeserializeObject<JObject>(reqStr);
+                if (obj == null || obj["moveHistory"] == null)
+                {
+                    return BadRequest("moveHistory not found");
+                }
+
+                List<Board> moveHistory = obj["moveHistory"].ToObject<List<Board>>();
+                if (moveHistory == null || moveHistory.Count == 0)
+                {
+                    return BadRequest("No move was made");
+                }
+
+                FilterDefinition<Game> filter = Game.GetFilterById(gameId);
+                List<Game> games = MongoConnection<Game>.Find(Collections.GAMES, filter);
+                if (games.Count == 0)
+                {
+                    return BadRequest("No game exists with the ID: " + gameId);
+                }
+
+                // Display the most recent moves first
+                moveHistory.Reverse();
+                games[0].MoveHistory.InsertRange(0, moveHistory);
+                MongoConnection<Game>.ReplaceOne(Collections.GAMES, games[0], filter);
+
+                return Ok(games[0]);
+            }
+
+            // Sent form data
+            else
+            {
+                return BadRequest("Must post data as content-type application/json");
+            }
+        }
     }
 }
